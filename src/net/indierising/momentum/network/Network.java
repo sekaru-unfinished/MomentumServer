92e9f1648ae1adfb85c27a3c346f7e8db904255f
Mali Coxon <malii.coxon@gmail.com>
2014-12-24 01:23:40 +0000

    Added basic server.

--- src/net/indierising/momentum/network/Network.java
+++ src/net/indierising/momentum/network/Network.java
@@ -4,8 +4,8 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.EndPoint;
 
 public class Network {
-	static public String ip = "localhost";
-	static public final int port = 9000;
+	public static String ip = "localhost";
+	public static int TCP_PORT = 9000, UDP_PORT = 9001;
 	
 	static public void register (EndPoint endPoint) {
 		Kryo kryo = endPoint.getKryo();
