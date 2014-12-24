e79960ae3f8caa476f2541495798a50788309bd5
Mali Coxon <malii.coxon@gmail.com>
2014-12-24 21:58:51 +0000

    Changed describe method slightly

--- src/net/indierising/momentum/Main.java
+++ src/net/indierising/momentum/Main.java
@@ -63,7 +63,6 @@ public class Main {
 		for(int i = 0; i < properties.size(); i++){
 			System.out.print(properties.get(i).getName() + " -- " + properties.get(i).getData() + "  --  ");
 			System.out.println(properties.get(i).getData().getClass().getName());
-			System.out.println("----");
 		}
 	}
 }
